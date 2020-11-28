2417c13acf641c6bf3719d3138dd9eea0fe0b014
IgorGuilhermeRocha <igorguilhermerocha@hotmail.com>
2020-11-28 17:15:46 -0300

    Teste

---
+++ src/matriz/E1.java
@@ -0,0 +1,15 @@
+package matriz;
+import java.util.Scanner;
+public class E1 {
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+		int m;
+		
+		int n;
+		System.out.printf("%d %d ",m = sc.nextInt(),n = sc.nextInt());
+		int[][] mat = new int[m][n];
+		
+		
+	}
+
+}
