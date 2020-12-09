7789c96fc500a0f0cf043b69eb0bca0713a33bae
IgorGuilhermeRocha <igorguilhermerocha@hotmail.com>
2020-12-09 11:25:41 -0300

    Sysout acrescentado

--- src/matriz/E1.java
+++ src/matriz/E1.java
@@ -3,11 +3,8 @@ import java.util.Scanner;
 public class E1 {
 	public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-		int m;
 		
-		int n;
-		System.out.printf("%d %d ",m = sc.nextInt(),n = sc.nextInt());
-		int[][] mat = new int[m][n];
+		System.out.println(" kkkkkkkkk");
 		
 		
 	}
